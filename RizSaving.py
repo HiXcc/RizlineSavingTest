cdec9d1021c594a4ab8a2d417f03f9dd04d0d56f
HiXcc <130841228+HiXcc@users.noreply.github.com>
2026-02-15 07:25:47 +0800

    Update RizSaving.py

--- RizSaving.py
+++ RizSaving.py
@@ -2,6 +2,7 @@ from Crypto.Cipher import AES
 import uuid
 import random
 import requests
+import base64
 
 def rizline_aes_decrypt(encrypt_data: bytes, key, iv):
     cipher = AES.new(key, AES.MODE_CBC, iv)
@@ -26,24 +27,30 @@ def get_save(phone,password,headers):
     return (res.status_code,res.text)
 
 def get_token(phone,password):
-  headers = {"game_id":"pigeongames.rizline","phone":phone,"device_id":str(uuid.uuid4()),"channel_id":str(random.randint(1,11))}
-  phone_exist = requests.post("https://rizserver.pigeongames.net/account/check_phone",json={"phone": phone},headers=headers).json()
-  if phone_exist["code"] == 0:
-      data = {"phone": phone,"password": password}
-      res = requests.post("https://rizserver.pigeongames.net/account/login",json=data,headers=headers)
-      res_json = res.json()
-      if res_json["code"] == 0:
-          token = res.headers["set_token"]
-          print("token:",res.headers["set_token"])
-      else:
-          print("r",res_json["msg"])
-  else:
-      verify_code_data = {"phone": phone,"transaction": "login"}
-      requests.post("https://rizserver.pigeongames.net/account/send_verify_code",json=verify_code_data,headers=headers)
-      code = int(input("riz 验证码"))
-      data = {"phone": phone,"code": code}
-      res = requests.post("https://rizserver.pigeongames.net/account/login",json=data,headers=headers)
-      token = res.headers["set_token"]
-      print("token:",res.headers["set_token"])
-  result = get_save(phone,password,token)
-  print(result)
+    headers = {"game_id":"pigeongames.rizline","phone":phone,"device_id":str(uuid.uuid4()),"channel_id":str(random.randint(1,11))}
+    phone_exist = requests.post("https://rizserver.pigeongames.net/account/check_phone",json={"phone": phone},headers=headers).json()
+    if phone_exist["code"] == 0:
+        data = {"phone": phone,"password": password}
+        res = requests.post("https://rizserver.pigeongames.net/account/login",json=data,headers=headers)
+        res_json = res.json()
+        if res_json["code"] == 0:
+            token = res.headers["set_token"]
+        else:
+            print("r",res_json["msg"])
+    else:
+        verify_code_data = {"phone": phone,"transaction": "login"}
+        requests.post("https://rizserver.pigeongames.net/account/send_verify_code",json=verify_code_data,headers=headers)
+        code = int(input("riz 验证码"))
+        data = {"phone": phone,"code": code}
+        res = requests.post("https://rizserver.pigeongames.net/account/login",json=data,headers=headers)
+        token = res.headers["set_token"]
+    headers.update({"token":token})
+    results = get_save(phone,password,headers)
+    for result in results:
+        if isinstance(result,requests.Response):
+            print(base64.b64decode(result.text))
+        else:
+            print(base64.b64decode(result))
+
+if __name__ == "__main__":
+    get_token("手机号","密码")
